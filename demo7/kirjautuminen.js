3a20d04be7454ec8124ec5b1ad1e6b8f3c159e57
AnastasiaTomar27 <anastasia.tomar@edu.keuda.fi>
2023-05-04 09:28:09 +0300

    demo9

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
 if(kirjautunut === 'kylla') {
     document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
-}
+} 
 }
 
 function kirjaudu(){

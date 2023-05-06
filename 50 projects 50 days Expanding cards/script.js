fbfc19a93e7bf24a80ebf39919feca0c238b199c
AnastasiaTomar27 <anastasia.tomar@edu.keuda.fi>
2023-05-06 18:14:41 +0300

    comments added

--- 50 projects 50 days Expanding cards/script.js
+++ 50 projects 50 days Expanding cards/script.js
@@ -1,14 +1,14 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel'); /*selects all objects this class "panel"*/ 
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active');
+panels.forEach(panel => { /*checks through the whole list of objects with class panel*/ 
+    panel.addEventListener('click', () => { /*when clicked it calls function*/
+        removeActiveClasses(); /*calls this function*/
+        panel.classList.add('active'); /*adds class panel to the selected object*/
     })
 })
 
 function removeActiveClasses(){
-    panels.forEach(panel => {
-        panel.classList.remove('active');
+    panels.forEach(panel => { /*goes through each of the panels*/
+        panel.classList.remove('active');/*removes class selected from the objects in the panels list*/
     })
 }
